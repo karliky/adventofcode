3d5ce6930ec94fc5b52be7d3b6d65c502d6d5bd1
Karliky <karliky@gmail.com>
2019-12-01 14:18:25 +0100

    add minimum code base

---
+++ 01/test/test.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+describe('Day 1: The Tyranny of the Rocket Equation', () => {
+  describe('Module and mass', () => {
+    it('should create a module with default mass', () => {
+      const module = new ModuleExplorer.Builder().build();
+      assert.equal(module.name, 'Nogg-Aholic explorer');
+      assert.equal(module.mass, 0);
+    });
+
+    it('should create a module with custom mass', () => {
+      const module = new ModuleExplorer.Builder()
+      .withMass(122)
+      .build();
+      assert.equal(module.name, 'Nogg-Aholic explorer');
+      assert.equal(module.mass, 122);
+    });
+  });
+});
+
+class ModuleExplorer {
+  constructor(build) {
+     this.name = build.name || 'Nogg-Aholic explorer';
+     this.mass = build.mass || 0;
+  }
+  static get Builder() {
+     class Builder {
+        constructor() {}
+        withName(name) {
+           this.name = name;
+           return this;
+        }
+        withMass(mass) {
+           this.mass = mass;
+           return this;
+        }
+        build() {
+           return new ModuleExplorer(this);
+        }
+     }
+     return Builder;
+  }
+}
